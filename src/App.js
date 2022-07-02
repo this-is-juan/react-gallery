a7b03b2ba950baf683baeca05b42db81bb4be6ad
Juan <j.sancho.arango@gmail.com>
2022-07-02 12:00:58 +0200

    Added images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./styles.css";
+import React from "react";
+import cabbage from "./assets/image1.jpeg";
+import mango from "./assets/image2.jpeg";
+import fig from "./assets/image3.jpeg";
+import gaze from "./assets/image4.jpeg";
+import peach from "./assets/image5.jpeg";
+import avocado from "./assets/image6.jpeg";
 
-export default function App() {
+const images = [cabbage, mango, fig, gaze, peach, avocado];
+
+const App = () => {
   return (
     <div className="App">
-      <h1>React-gallery</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="title">
+        <h1>Petricor</h1>
+        <h2>A photo gallery built on React</h2>
+      </div>
+      <div className="image-gallery">
+        <img alt="" src={images[2]} />
+      </div>
     </div>
   );
-}
+};
+
+export default App;
